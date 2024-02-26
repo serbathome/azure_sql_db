6203e19d9bb4f44bcae6d4f2869f0be2ec6add2f
Sergey Biryukov <serbathome@gmail.com>
2024-02-26 17:20:38 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,2 @@
 #  state file configuration
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "storage-accounts"
-    storage_account_name = "camptfstates"
-    container_name       = "tfstates"
-    key                  = "dbstate.tfstate"
-  }
-}
+
